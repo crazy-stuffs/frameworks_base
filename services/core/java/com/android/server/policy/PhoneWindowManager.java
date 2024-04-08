16bd99705e8a5de65f43b38aff595087fa11aaa8
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-08 08:21:51 +0800

    services: Introduce Shake Gestures
    Co-authored-by: AmeChanRain <downloadbot007@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -269,6 +269,8 @@ import lineageos.providers.LineageSettings;
 import org.lineageos.internal.buttons.LineageButtons;
 import org.lineageos.internal.util.ActionUtils;
 
+import org.rising.server.ShakeGestureService;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -677,6 +679,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mPendingCapsLockToggle;
 
     private int mForceNavbar = -1;
+    private ShakeGestureService mShakeGestures;
 
     // Tracks user-customisable behavior for certain key events
     private Action mBackLongPressAction;
@@ -885,7 +888,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             AssistUtils.INVOCATION_TYPE_ASSIST_BUTTON);
                     break;
                 case MSG_LAUNCH_VOICE_ASSIST_WITH_WAKE_LOCK:
-                    launchVoiceAssistWithWakeLock(true);
+                    launchVoiceAssistWithWakeLock();
                     break;
                 case MSG_SHOW_PICTURE_IN_PICTURE_MENU:
                     showPictureInPictureMenuInternal();
@@ -2267,7 +2270,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 logKeyboardSystemsEvent(event, KeyboardLogEvent.LAUNCH_ASSISTANT);
                 break;
             case VOICE_SEARCH:
-                launchVoiceAssistWithWakeLock(true);
+                launchVoiceAssistWithWakeLock();
                 break;
             case IN_APP_SEARCH:
                 triggerVirtualKeypress(KeyEvent.KEYCODE_SEARCH);
@@ -6215,7 +6218,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             mHandler.removeMessages(MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK);
             mHavePendingMediaKeyRepeatWithWakeLock = false;
-            mBroadcastWakeLock.release(); // pending repeat was holding onto the wake lock
+            if (mBroadcastWakeLock != null && mBroadcastWakeLock.isHeld()) {
+                mBroadcastWakeLock.release(); // pending repeat was holding onto the wake lock
+            }
         }
 
         dispatchMediaKeyWithWakeLockToAudioService(event);
@@ -6229,7 +6234,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             msg.setAsynchronous(true);
             mHandler.sendMessageDelayed(msg, ViewConfiguration.getKeyRepeatTimeout());
         } else {
-            mBroadcastWakeLock.release();
+            if (mBroadcastWakeLock != null && mBroadcastWakeLock.isHeld()) {
+                mBroadcastWakeLock.release();
+            }
         }
     }
 
@@ -6243,7 +6250,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         dispatchMediaKeyWithWakeLockToAudioService(repeatEvent);
-        mBroadcastWakeLock.release();
+        if (mBroadcastWakeLock != null && mBroadcastWakeLock.isHeld()) {
+            mBroadcastWakeLock.release();
+        }
     }
 
     void dispatchMediaKeyWithWakeLockToAudioService(KeyEvent event) {
@@ -6252,7 +6261,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    void launchVoiceAssistWithWakeLock(boolean withWakelock) {
+    void launchVoiceAssistWithWakeLock() {
         sendCloseSystemWindows(SYSTEM_DIALOG_REASON_ASSIST);
 
         final Intent voiceIntent;
@@ -6267,7 +6276,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             voiceIntent.putExtra(RecognizerIntent.EXTRA_SECURE, true);
         }
         startActivityAsUser(voiceIntent, UserHandle.CURRENT_OR_SELF);
-        if (withWakelock) {
+        if (mBroadcastWakeLock != null && mBroadcastWakeLock.isHeld()) {
             mBroadcastWakeLock.release();
         }
     }
@@ -6348,6 +6357,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mPocketMode != null) {
             mPocketMode.onInteractiveChanged(false);
         }
+        if (mShakeGestures != null) {
+            mShakeGestures.onInteractiveChanged(false);
+        }
     }
 
     // Called on the PowerManager's Notifier thread.
@@ -6423,6 +6435,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         if (mPocketMode != null) {
             mPocketMode.onInteractiveChanged(true);
+        }
+        if (mShakeGestures != null) {
+            mShakeGestures.onInteractiveChanged(true);
         }
     }
 
@@ -6872,6 +6887,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mVrManagerInternal != null) {
             mVrManagerInternal.addPersistentVrModeStateListener(mPersistentVrModeListener);
         }
+        
+        GestureCallbacks gestureCallbacks = new GestureCallbacks(mContext, mCurrentUserId);
+
+        mShakeGestures = ShakeGestureService.getInstance(mContext, (ShakeGestureService.ShakeGesturesCallbacks) gestureCallbacks);
+        mShakeGestures.onStart();
 
         mLineageHardware = LineageHardwareManager.getInstance(mContext);
         // Ensure observe happens in systemReady() since we need
@@ -6898,7 +6918,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mPocketMode.onStart();
     }
 
-    class GestureCallbacks implements SwipeToScreenshotListener.Callbacks, ShakeGestureService.ShakeGesturesCallbacks {
+    public class GestureCallbacks implements SwipeToScreenshotListener.Callbacks, ShakeGestureService.ShakeGesturesCallbacks {
         private Context mContext;
         private int mCurrentUserId;
 
@@ -6909,7 +6929,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         public void onVoiceLaunch() {
-            launchVoiceAssistWithWakeLock(false);
+            launchVoiceAssistWithWakeLock();
         }
 
         @Override
@@ -8065,6 +8085,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case AudioManager.RINGER_MODE_SILENT:
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 break;
+        }
+    }
+
+    private void turnScreenOnOrOff() {
+        if (mPowerManager.isInteractive()) {
+            mPowerManager.goToSleep(SystemClock.uptimeMillis());
+        } else {
+            mBroadcastWakeLock.acquire();
+            mWindowWakeUpPolicy.wakeUpFromWakeGesture();
+            if (mBroadcastWakeLock != null && mBroadcastWakeLock.isHeld()) {
+                mBroadcastWakeLock.release();
+            }
         }
     }
 
