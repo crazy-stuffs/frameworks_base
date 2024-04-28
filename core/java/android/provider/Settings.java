91e2d79bfa0c661a781236b3e49bac93fb0c8ad0
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-28 08:22:52 +0800

    SystemUI: Disable FaceUnlock Lockouts [1/2]
    [someone5678]
    * Make it togglable
    
    Change-Id: I371e75d4b45854586d34897ed3cf739542799a5d
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7093,6 +7093,12 @@ public final class Settings {
         }
 
         /**
+         * Enable/Disable face lockout
+         * @hide
+         */
+        public static final String FACE_LOCKOUT = "face_lockout";
+
+        /**
          * When to use Wi-Fi calling
          *
          * @see android.telephony.TelephonyManager.WifiCallingChoices
